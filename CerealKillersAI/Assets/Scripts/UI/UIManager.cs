0b8533cd6ce4471f4b651dc89189dd68a3ada083
Nova2311 <36200921+Nova2311@users.noreply.github.com>
2019-02-01 14:43:07 +0000

    UI
    Load of UI bollocks including selecting characters, teams, play/stop button

---
+++ CerealKillersAI/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager: MonoBehaviour {
+
+    public int teamIDNumber = 1;
+    //public int gameSpeedMultiplier = 1;
+
+    public GameObject teamPlacementPanel;
+
+    public void TroopSelected(GameObject selected) {
+        Debug.Log(selected.name);
+
+        /*
+        show the placement green/red
+
+        */
+    }
+
+    public void ChangeTeam(int TeamID) {
+        teamIDNumber = TeamID;
+        Debug.Log(TeamID.ToString());
+    }
+
+    public void StartGame() {
+        Debug.Log("Starting Simulation");
+        teamPlacementPanel.SetActive(false);
+    }
+
+    public void StopGame() {
+        Debug.Log("Stopping Simulation");
+        teamPlacementPanel.SetActive(true);
+    }
+
+    public void ChangeGameSpeed(int NewSpeed) {
+
+
+    }
+}
