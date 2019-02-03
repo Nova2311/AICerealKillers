d3fdf9800622621f22a7ea6fb9a729603091d097
kesada7 <kesadacastillero@gmail.com>
2019-02-03 17:59:31 +0000

    Camera RTS and multiple AI agent

---
+++ CerealKillersAI/Assets/Scripts/Player/PlayerPathFinding.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerPathFinding : MonoBehaviour {
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+
+            }
+        }
+
+	}
+}
