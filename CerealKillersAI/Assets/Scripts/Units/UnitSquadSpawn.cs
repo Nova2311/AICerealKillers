9c0ceee06cf521a49884eb172f43ea81097ffba9
Ryan McDonnell <ryjmc@outlook.com>
2019-02-22 01:29:38 +0000

    Uint squad spawn in
    we can now spawn in a squad of units onto the map based of the mouse position.
    
    demo location in ryans test scene.
     controles:
    -left click to place (or start the placeing is previousely deselected)
    -press escape to deselect and stop placeing

---
+++ CerealKillersAI/Assets/Scripts/Units/UnitSquadSpawn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+public class UnitSquadSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] placeableObjectPrefabs;
+
+    private GameObject currentPlaceableObject;
+
+    private float mouseWheelRotation;
+    private int currentPrefabIndex = -1;
+
+    private void Start()
+    {
+        for (int i = 0; i < placeableObjectPrefabs.Length; i++)
+        {
+            currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
+            currentPrefabIndex = i;
+        }
+    }
+
+    private void Update()
+    {
+        HandleNewObject();
+        ChechForDeselect();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+        }
+    }
+
+    private void ChechForDeselect()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //end the selection for this unit type
+            Destroy(currentPlaceableObject);
+            currentPlaceableObject = null;
+            return;
+        }
+    }
+
+    private void HandleNewObject()
+    {
+        for (int i = 0; i < placeableObjectPrefabs.Length; i++)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {  
+                    currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
+                    currentPrefabIndex = i;             
+            }
+        }
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+}
