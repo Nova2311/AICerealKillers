584fd602465e44031b768d0ce9ec8a41037b7cd9
Nova2311 <jonnyfairlamb@hotmail.com>
2019-03-01 13:48:25 +0000

    Complete Capture System
    Finished the capture system - "Flag" changes colour depending on who
    controls the point

--- CerealKillersAI/Assets/Scripts/Map/CapturePoint.cs
+++ CerealKillersAI/Assets/Scripts/Map/CapturePoint.cs
@@ -4,91 +4,121 @@ using UnityEngine;
 
 public class CapturePoint : MonoBehaviour {
 
-    public float CaptureScore = 0;
+    public float blueCaptureScore = 0;
+    public float redCaptureScore = 0;
     private float coolDown = 3;
-    private bool tied = false;
 
-    private bool nutrual = true;
-    private bool red = false;
-    private bool blue = false;
+    private bool blueOnPoint = false;
+    private bool redOnPoint = false;
+    public bool contested = false;
+
+    public bool neutral = true;
+    public bool redControlled = false;
+    public bool blueControlled = false;
 
 
     public GameObject flag;
     public int CaptureVariable = 10;
+    public Color redTeam;
+    public Color blueTeam;
+    public Color neutralColour;
 
-    void Start()
-    {
-
+    private void Start() {
+        flag.GetComponent<Renderer>().material.color = neutralColour;
     }
 
-    void Update()
-    {
-        if (CaptureScore > 100)
-            CaptureScore = 100;
+    void Update(){
 
-        if (CaptureScore < 0)
-            CaptureScore = 0;
-    }
-
-    void OnTriggerEnter(Collider col)
-    {
-        /* if (col.gameObject.tag == "Blue" && col.gameObject.tag == "Red")
-          {
-              tied = true;
-          }
-          else tied = false;
-          */
-        Debug.Log(col.gameObject.name);
-        if (tied == false) // to check if both the team is in the circle if so the captureing stops until one team is eliminated
-         {
-            Debug.Log("wut");
-            if (col.gameObject.tag == "Blue")
-            {
-                Debug.Log("wutt");
-                if (nutrual == true)
-                {
-                    CaptureScore = CaptureScore +  CaptureVariable;
-                    Debug.Log("wuttt");
-                }
-                else if (red == true)
-                { CaptureScore = CaptureScore - Time.deltaTime * CaptureVariable; }
-                else if (blue == true)
-                { CaptureScore = CaptureScore + Time.deltaTime * CaptureVariable; }
-            }
-
-            if (col.gameObject.tag == "Red")
-            {
-                if (nutrual == true)
-                { CaptureScore = CaptureScore + Time.deltaTime * CaptureVariable; }
-                else if (red == true)
-                { CaptureScore = CaptureScore + Time.deltaTime * CaptureVariable; }
-                else if (blue == true)
-                { CaptureScore = CaptureScore - Time.deltaTime * CaptureVariable; }
-            }
+        if (blueOnPoint && redOnPoint) {
+            contested = true;
+        } else {
+            contested = false;
         }
 
-        //if (col.gameObject.tag == "Blue" && CaptureScore == 100 && nutrual == true)
-        //{
-        //    nutrual = false;
-        //    blue = true;
-        //    Debug.Log("blue has captured a point");
-        //}
-        //if (col.gameObject.tag == "Red" && CaptureScore == 100 && nutrual == true)
-        //{
-        //    nutrual = false;
-        //    red = true;
-        //    Debug.Log("red has captured a point");
-        //}
-        //if (col.gameObject.tag == "Blue" && CaptureScore == 0 && nutrual == true)
-        //{
-        //    nutrual = false;
-        //    red = false;
-        //}
-        //if (col.gameObject.tag == "Red" && CaptureScore == 0 && nutrual == true)
-        //{
-        //    nutrual = true;
-        //    blue = false;
-        //}
+        if (blueCaptureScore > 100) {
+            blueCaptureScore = 100;
+        }
+        if (redCaptureScore > 100) {
+            redCaptureScore = 100;
+        }
+
+        if (blueCaptureScore < 0) {
+            blueCaptureScore = 0;
+            blueControlled = false;
+            neutral = true;
+            flag.GetComponent<Renderer>().material.color = neutralColour;
+        }
+        if (redCaptureScore < 0) {
+            redCaptureScore = 0;
+            redControlled = false;
+            neutral = true;
+            flag.GetComponent<Renderer>().material.color = neutralColour;
+        }
+
+        if (BlueCapturedPoint()) {
+            blueControlled = true;
+            neutral = false;
+            flag.GetComponent<Renderer>().material.color = blueTeam;
+        }
+        if (RedCapturedPoint()) {
+            redControlled = true;
+            neutral = false;
+            flag.GetComponent<Renderer>().material.color = redTeam;
+        }
+    }
+
+    void OnTriggerStay(Collider col){
+        if (col.gameObject.tag == "Blue") {
+            blueOnPoint = true;
+        }
+        if (col.gameObject.tag == "Red") {
+            redOnPoint = true;
+        }
+          
+        if (contested == true) // to check if both the team is in the circle if so the captureing stops until one team is eliminated
+            return;
+            if (col.gameObject.tag == "Blue"){
+                if (neutral == true && redCaptureScore == 0f) {
+                    blueCaptureScore = blueCaptureScore + CaptureVariable * Time.deltaTime;
+                } else if (redControlled == true || redCaptureScore != 0f) {
+                    redCaptureScore = redCaptureScore - Time.deltaTime * CaptureVariable;
+                   
+                }
+            }
+
+            if (col.gameObject.tag == "Red"){
+                if (neutral == true && blueCaptureScore == 0f) {
+                    redCaptureScore = redCaptureScore +  CaptureVariable * Time.deltaTime;
+                } else if (blueControlled == true || blueCaptureScore != 0f) {
+                    blueCaptureScore = blueCaptureScore -= CaptureVariable * Time.deltaTime;
+                    
+                }
+            }
+    }
+
+    private void OnTriggerExit(Collider col) {
+        if (col.gameObject.tag == "Blue") {
+            blueOnPoint = false;
+        }
+        if (col.gameObject.tag == "Red") {
+            redOnPoint = false;
+        }
+    }
+
+    bool BlueCapturedPoint() {
+        if (blueCaptureScore == 100 && !contested) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool RedCapturedPoint() {
+        if (redCaptureScore == 100 && !contested) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
