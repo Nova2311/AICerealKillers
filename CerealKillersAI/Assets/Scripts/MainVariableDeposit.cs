d5ecc9eaf4cd3815f8f0e4a5ba7b42fb8ca5162b
Adrian Tesdal <actesdal@gmail.com>
2019-03-11 15:00:09 +0000

    Added buildingname, team, and unitname enums

--- CerealKillersAI/Assets/Scripts/MainVariableDeposit.cs
+++ CerealKillersAI/Assets/Scripts/MainVariableDeposit.cs
@@ -6,24 +6,43 @@ using UnityEngine;
  all in one script instaid of includeing multiple at once we will only need one, and it will make it easyier to modify these
  variables in a rush as they are all together.*/
 
+public enum Team
+{
+	Red,
+	Blue
+};
+
+public enum BuildingName
+{
+	Building1,
+	Building2,
+	Building3,
+	Building4
+}
+
+public enum UnitName
+{
+	Worker,
+	LightSword,
+	LightAxe,
+	HeavySword,
+	HeavyAxe,
+	Archer1,
+	Archer2,
+	Spear1,
+	Spear2,
+	LightCav,
+	HeavyCav,
+	Mage1,
+	Mage2,
+	Priest,
+	Catapult
+}
 
 public class MainVariableDeposit : MonoBehaviour
 {
-
-
-    private int GameSpeed = 1;
-    private float GameCountDownTimer = 0;
-    private int PlayerGoald = 0;
-    private int AIGoald = 0;
-
-    // Use this for initialization
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+	private int GameSpeed = 1;
+	private float GameCountDownTimer = 0;
+	private int PlayerGoald = 0;
+	private int AIGoald = 0;
 }
